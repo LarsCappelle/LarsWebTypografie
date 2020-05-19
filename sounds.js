7d4df319e788815a538d581bf40cb6b10ee6a569
LarsCappelle <63396655+LarsCappelle@users.noreply.github.com>
2020-05-19 13:12:32 +0200

    Add files via upload

--- sounds.js
+++ sounds.js
@@ -6,11 +6,15 @@ sounds = [
 	9,      //class sound2
 	12,     //class sound3
 	14,     //class sound4
-	15,        //class sound5
-	16,        //class sound6
-	25.8,   //class sound7
-	31,        //class sound8
-	32.8,        //class sound9
-	68,        //class sound10
-	75         //class sound11
+	15,         //class sound5
+	16,         //class sound6
+	24.8,       //class sound7
+	31,         //class sound8
+	32.8,       //class sound9
+	68,         //class sound10 uplifting rood scherm
+	75,         //class sound11 scherm weer zwart
+    87.6,       //class sound12 weer naar licht scherm
+    103,        //class sound13 weer naar donker scherm
+    109,        //class sound14 weer naar licht scherm
+    116,        //class sound15 weer naar donker scherm
 ];
