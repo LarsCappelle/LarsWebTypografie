5d3cc9e3028a7ef77cf4ed177b4deaab81f7877e
LarsCappelle <63396655+LarsCappelle@users.noreply.github.com>
2020-05-26 20:41:38 +0200

    Add files via upload

--- sounds.js
+++ sounds.js
@@ -10,15 +10,19 @@ sounds = [
 	16,         //class sound6
 	24.8,       //class sound7
 	31,         //class sound8
-	32.8,       //class sound9
+	32.8,       //class sound9 begin overhoring pieptoon
 	68,         //class sound10 uplifting rood scherm
-	75,         //class sound11 scherm weer zwart
-    87.6,       //class sound12 weer naar licht scherm
-    86,         //class sound13 muziek
-    103,        //class sound14 weer naar donker scherm
-    109,        //class sound15 weer naar licht scherm
-    115.8,        //class sound16 weer naar donker scherm
-    118.3,        //class sound17 blauwe gloed
-    122.5,        //class sound18 weer naar donker scherm
-    129.5,        //class sound19 silence
+	75,         //class sound11 silence zwart scherm
+    86,         //class sound12 mysterieuze muziek
+    103,        //class sound13 message bij vrouw achter pc
+    113,        //class sound14 muziek wordt harder
+    129.5,      //class sound15 muziek stopt
+    134         //class sound16
+    
+//    103,        //class sound14 weer naar donker scherm
+//    109,        //class sound15 weer naar licht scherm
+//    115.8,        //class sound16 weer naar donker scherm
+//    118.3,        //class sound17 blauwe gloed
+//    122.5,        //class sound18 weer naar donker scherm
+//    129.5,        //class sound19 silence
 ];
